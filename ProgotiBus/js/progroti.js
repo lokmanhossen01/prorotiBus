91c58e2cf32c20f139a653a3f05adce8bc7e19bb
lokmanhossen01 <https://github.com/lokmanhossen01>
2024-02-19 18:39:15 +0600

    added bestOffer Section

---
+++ ProgotiBus/js/progroti.js
@@ -0,0 +1,6 @@
+const kbd = document.getElementsByClassName('kbd')
+for (const button of kbd) {
+    button.addEventListener('click', function(show) {
+        const seat = show.target
+    })    
+}
